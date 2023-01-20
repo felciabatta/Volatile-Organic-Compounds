78e8f9b434afd7a9d51dcd01c1f7dae236796c79
FelixxDP <felix.dp.2020@bristol.ac.uk>
2023-01-20 16:24:34 +0000

    updated dataselect to include plotting, correlation and ratio funtionality, removed anomolies from data

--- DataCorrection.py
+++ DataCorrection.py
@@ -11,34 +11,41 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 import numpy as np
 
+# import all spreadsheets separate data frames
 data = pd.read_excel("../LMR_VOCdata_97-19_DOW.xlsx", None,
                      na_values='No data', parse_dates=False)
-
+# concatenate dataframes into one
 data = [data[k] for k in list(data.keys())]
-
 data = pd.concat(data)
-
-data.to_csv("../LMR_VOCdata_97-19_DOW_TEMP.csv")
-
+# export to csv
+data.to_csv("../LMR_VOCdata_97-19_DOW_TEMP.csv", index=False)
+# read from csv
 data = pd.read_csv("../LMR_VOCdata_97-19_DOW_TEMP.csv")
 
 # the midnight times are set incorrectly at the following points indexes
 midnightErrLoc = (data.Time == "1900-01-01 00:00:00") | (data.Time == "1900-01-02 00:00:00")
-
+# set inccorect times to midnight value
 data.Time[midnightErrLoc] = "00:00:00"
-
-data.Datetime = data.Date + " " + data.Time
-
+# create datetime string column
+data["Datetime"] = data.Date + " " + data.Time
+# convert datetime strings to datetime format
 data.Datetime = pd.to_datetime(data.Datetime)
-
+# add 1 day to each of the inccorect dates (as originally formatted 1 day off)
 data.Datetime[midnightErrLoc] = data.Datetime[midnightErrLoc] + pd.Timedelta(days=1)
-
+# remove unneeded columns
 data = data.drop(["Date", "Time", "DOW", "Unnamed: 0"], 1)
-
+# rearrange columns
 cols = ["Datetime"] + data.columns.to_list()[:-1]
-
 data = data[cols]
-
+# set datetime index
 data = data.set_index(['Datetime'])
 
-data.to_csv("LMR_VOCdata_97-19_DOW.csv", na_rep="NaN")
+if 1:
+    # strange anomlies occuring at values below 0.05, so remove them
+    data[data<0.05] = np.nan
+
+    # export to csv
+    data.to_csv("LMR_VOCdata_97-19_DOW_W.csv", na_rep="NaN")
+else:
+    # export to csv, WITH ANOMALIES
+    data.to_csv("LMR_VOCdata_97-19_DOW_W_ANOMALY.csv", na_rep="NaN")
--- DataSelect.py
+++ DataSelect.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+from sklearn.linear_model import LinearRegression
+import matplotlib as mpl
+import seaborn as sns
+from scipy import stats as st
 
 
 class Plot:
@@ -94,22 +98,34 @@ class Plot:
 
 
 class vocData():
+    """
+    Time series dataframe object with extended data filtering capabilities.
+
+    Parameters
+    ----------
+    csvdata : .csv, optional
+        VOC/time series data used. Must have 'Datetime' column with
+        correctly formatted dates.
+        The default is "LMR_VOCdata_97-19_DOW.csv".
+
+    Returns
+    -------
+    vocData object.
+
+    Examples
+    --------
+
+    Initialise using
+
+    >>> data = vocData()
+
+    Select, filter and group using
+
+    >>> data.select(timerange, mon_filter, dow_filter, hr_filter, groupby, aggmethod)
+
+    """
+
     def __init__(self, csvdata="LMR_VOCdata_97-19_DOW.csv"):
-        """
-        Time series data object with extended data filtering capabilities.
-
-        Parameters
-        ----------
-        csvdata : .csv, optional
-            VOC/time series data used. Must have 'Datetime' column with
-            correctly formatted dates.
-            The default is "LMR_VOCdata_97-19_DOW.csv".
-
-        Returns
-        -------
-        vocData object.
-
-        """
         # TODO: BETTER TO INHERET from pandas dataframe class first
         self.data = pd.read_csv(
             csvdata, na_values='No data', index_col="Datetime",
@@ -127,26 +143,64 @@ class vocData():
         else:
             return unitindexes
 
-    def select(self, timerange=None, mon_filter=None, dow_filter=None,
-               hr_filter=None):
-        """
-        Selects
+    def select(self, timerange: (str, list) = None,
+               mon_filter: (int, list) = None,
+               dow_filter: (int, list) = None,
+               hr_filter: (int, list) = None,
+               groupby: {'Y', 'M', 'D'}=None,
+               aggmethod: {'mean', 'sum'}='mean') -> pd.DataFrame:
+        """Select, filter and group data.
+
+        Select data within timerange, subject to filter by month, day of week or hour.
+        Can also aggregate data by year, month or day.
 
         Parameters
         ----------
-        timerange : TYPE, optional
-            DESCRIPTION. The default is None.
-        mon_filter : TYPE, optional
-            DESCRIPTION. The default is None.
-        dow_filter : TYPE, optional
-            DESCRIPTION. The default is None.
-        hr_filter : TYPE, optional
-            DESCRIPTION. The default is None.
+        timerange : ("YYYY-MM-DD hh:mm:ss", list), optional
+            YYYY to select whole year, YYYY-MM to select whole month, etc.
+            Use list of two dates to select a range.
+            The default is None, which selects entire dataset.
+        mon_filter : (int, list), optional
+            In addition to time range selection, filter out only specific month.
+            e.g. `1` will extract all Januarys only.
+            The default is None, extracting all months.
+        dow_filter : (int, list), optional
+            In addition to time range selection, filter out only specific day of week.
+            e.g. `1` will extract all Mondays only.
+            The default is None, extracting all dows.
+        hr_filter : (int, list), optional
+            In addition to time range selection, filter out only specific hour of day.
+            e.g. `13` will extract all 1pms only.
+            The default is None, extracting all hours.
+        groupby : {'Y', 'M', 'D'}, optional
+            Aggregates by grouping of year `'Y'`, month `'M'` or day `'D'`, according to `aggmethod`.
+            The default is None.
+        aggmethod : {'mean', 'sum'}, optional
+            Aggregation  method.
+            The default is 'mean'.
 
         Returns
         -------
-        dataslice : TYPE
-            DESCRIPTION.
+        dataslice : pandas.dataframe
+            Selected data.
+
+        Examples
+        --------
+        Select 1pm Feb 2003 to Aug 2018, group/aggregated by day:
+
+        >>> data.select(["2003-02 13", "2018-08"], groupby='D')
+
+        Select all Tuesdays in Feb 2003:
+
+        >>> data.select("2003-02", dow_filter=1)
+
+        Select all Tuesdays and Sundays in both Aug and Nov of 2003:
+
+        >>> data.select("2003", mon_filter=[8, 11], dow_filter=[1, 6])
+
+        Select all 12ams and 12pms, and group by month, aggregated with sum
+
+        >>> data.select(hr_filter=[0 12], groupby='M', aggmethod='sum')
 
         """
         if likelist(timerange):
@@ -165,9 +219,12 @@ class vocData():
             if notNon(f):
                 dataslice = self._filtbyunit(dataslice, f, self.units[i+1])
 
+        if notNon(groupby):
+            dataslice = self._group_by(groupby, dataslice, aggmethod)
+
         return dataslice
 
-    def group_by(self, unit=None, data=None, aggmethod='mean'):
+    def _group_by(self, unit=None, data=None, aggmethod='mean'):
         """
 
 
@@ -206,6 +263,97 @@ class vocData():
 
         return data[loc]
 
+    def corrmat(self, data=None, H0=0, tail='up', plot=0, sort_index=None):
+        if no(data):
+            data = self.data
+        # correlation matrix
+        corr = data[VOCS].corr(method='spearman')
+        if no(sort_index):
+            # sort by mean r-value
+            sort_index = np.argsort(np.mean(corr)).to_list()
+        corr = corr[corr.columns[sort_index]].loc[corr.columns[sort_index]]
+        # corr, p = st.spearmanr(data[vocs], nan_policy='omit')
+
+        # no. of data points
+        n = len(data)
+        # null hypothesis: assumed spearman mean
+        mu = H0
+        # fisher transformation - use abs as interested prescence correlation
+        f = np.arctanh(abs(corr))
+        muf = np.arctanh(mu)  # fisher trans. mean
+        # standard normal
+        z = (f - muf)*np.sqrt(n-3)
+        # p-values (cumulative probabilities)
+        p = st.norm.cdf(z)
+
+        # critical value
+        crit = 0.01
+        # no. statistical tests (= no. correlations)
+        m = (len(corr)-1)*len(corr)/2
+        # bonferroni corrected critical value
+        critl = crit/m
+        critu = 1-crit
+
+        # determine which are statistically significant
+        if tail == 'two':
+            significant = (p <= critl/2) & ((1-p) <= critl/2)
+        elif tail == 'low':
+            significant = (p <= critl)
+        elif tail == 'up':
+            significant = (1-p <= critl)
+
+        if plot:
+            # Sample figsize in inches
+            fig, ax = plt.subplots(figsize=(10, 7.5))
+            sns.heatmap(corr, cmap="coolwarm", vmin=-1, vmax=1,
+                        xticklabels=True, yticklabels=True)
+            # Sample figsize in inches
+            fig, ax = plt.subplots(figsize=(10, 7.5))
+            sns.heatmap(significant,
+                        xticklabels=True, yticklabels=True)
+        return corr, p, significant, sort_index
+
+    def correlate(self, col1, col2, data=None, plot=False):
+        if no(data):
+            data = self.data
+        data1 = data[col1]
+        data2 = data[col2]
+        corr = data1.corr(data2)
+
+        if plot:
+            plt.figure()
+            plt.scatter(data1, data2, s=.1)
+            plt.xlabel(col1)
+            plt.ylabel(col2)
+        return corr
+
+    def ratio(self, col1, col2, data=None, inv=False, plot=False):
+        if no(data):
+            data = self.data
+        if inv:
+            data1 = data[col2]
+            data2 = data[col1]
+        else:
+            data1 = data[col1]
+            data2 = data[col2]
+        q = data1/data2
+
+        if plot:
+            self.scatter(q, title=data1.name+':'+data2.name)
+        return q
+
+    def scatter(self, data=None, title=None):
+        if no(data):
+            data = self.data
+        if no(title):
+            title = data.name
+        dates = data.index
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(dates, data, s=1)
+        plt.xlabel('Time')
+        plt.title(title)
+
 
 def likelist(item):
     return type(item) == tuple or type(item) == list
@@ -222,6 +370,40 @@ def notNon(item):
 def no(item):
     return item is None
 
+
+VOCS = ['Nitric oxide',
+        'Nitrogen dioxide',
+        'Sulphur dioxide',
+        'Carbon monoxide',
+        'ethane',
+        'ethene',
+        'ethyne',
+        'propane',
+        'propene',
+        'iso-butane',
+        'n-butane',
+        '1-butene',
+        'trans-2-butene',
+        'cis-2-butene',
+        'iso-pentane',
+        'n-pentane',
+        '1,3-butadiene',
+        'trans-2-pentene',
+        'cis-2-pentene (VOC-AIR only)', # ONLY TO 2005
+        '2-methylpentane',
+        '3-methylpentane (VOC-AIR only)', # ONLY TO 2005
+        'isoprene',
+        'n-hexane',
+        'n-heptane',
+        'benzene',
+        'toluene',
+        'ethylbenzene',
+        'm+p-xylene',
+        'o-xylene',
+        '1,2,4-trimethylbenzene',
+        '1,3,5-trimethylbenzene']
+# strange anomaly occurs around 2013-2015 at near 0 value (<0.05)
+# for almost all VOCS - only top 5 doesn't appear
 
 # Enter selected year and time window
 # p1 = Plot("2015", "Daily")
