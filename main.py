e593ff6ddf5fe529bf7dec686f992836fa99e7f5
FelixxDP <felix.dp.2020@bristol.ac.uk>
2023-02-07 12:27:41 +0000

    added sorting mechanism for corr mats, added sorted corr mat figs

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for voc in VOCS:
 
 # %% CORRELATE from COMPONENTS
 
-seasonality = 'trend'
+seasonality = 'daily'
 time_unit = 'H'
 
 component_files = ['Results/'+voc+'_components_fittedby_'+time_unit+'.csv' for voc in VOCS]
@@ -25,4 +25,10 @@ for i, df1 in enumerate(component_frames):
         cols = ['ds', seasonality]
         C[i, j], _ = data.pct_corr(df1[cols], df2[cols], log=True)
 
-sns.heatmap(C, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=VOCS, yticklabels=VOCS)
+# sort rows & columns
+sort_index = np.argsort(np.nanmean(C, axis=1)).tolist()
+C_sorted = C[sort_index, :][:, sort_index]
+VOCS_sorted = np.array(VOCS)[sort_index].tolist()
+# sns.heatmap(C, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=VOCS, yticklabels=VOCS)
+# plt.figure()
+sns.heatmap(C_sorted, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=VOCS_sorted, yticklabels=VOCS_sorted)
