36de32feb29c1a92f7e19df8b2b7fb7fec238259
FelixxDP <felix.dp.2020@bristol.ac.uk>
2023-02-10 00:34:59 +0000

    save heatmap pdf with titles

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# %% Initialise Packages
 from DataSelect import vocData, VOCS, unpickle
 
 import pandas as pd
@@ -15,7 +16,7 @@ for voc in VOCS:
 
 # %% EXTRACT COMPONENTS and MODELS
 seasonality = 'daily'
-time_unit = 'D'
+time_unit = 'H'
 
 component_files = ['Results/'+voc+'_components_fittedby_'+time_unit+'.csv' for voc in VOCS]
 component_frames = [pd.read_csv(file, index_col=0, parse_dates=[1]) for file in component_files]
@@ -28,7 +29,7 @@ C = np.empty((len(VOCS), len(VOCS)))
 for i, df1 in enumerate(component_frames):
     for j, df2 in enumerate(component_frames):
         cols = ['ds', seasonality]
-        C[i, j], _ = data.pct_corr(df1[cols], df2[cols], log=True)
+        C[i, j], _ = data.pct_corr(df1[cols], df2[cols], log=True, pct_freq='H')
 
 # sort rows & columns
 sort_index = np.argsort(np.nanmean(C, axis=1)).tolist()
@@ -36,7 +37,10 @@ C_sorted = C[sort_index, :][:, sort_index]
 VOCS_sorted = np.array(VOCS)[sort_index].tolist()
 # sns.heatmap(C, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=VOCS, yticklabels=VOCS)
 # plt.figure()
-sns.heatmap(C_sorted, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=VOCS_sorted, yticklabels=VOCS_sorted)
+sns.heatmap(C_sorted, cmap='coolwarm', vmin=-1, vmax=1, yticklabels=VOCS_sorted, xticklabels=[])
+plt.title(seasonality+" correlations")
+plt.savefig("Figures/pct_corrs/pctcorr_"+seasonality+"by_"+time_unit+"_sorted.pdf")
+
 
 # %% MAKE PROPHET PLOTS from MODELS
 for i, m in enumerate(models):
